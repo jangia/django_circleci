aaed957dca0cbcd9cbb320f22a8d3ad3d34ea77c
Jan Giacomelli <jan@typless.com>
2020-03-13 22:56:23 +0100

    Changed settings

--- django_circleci/settings.py
+++ django_circleci/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_circleci.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'example',
+        'USER': 'example',
+        'PASSWORD': 'example',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
